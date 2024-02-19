561b4039fd2fd8171bc50cbc47fd7fa9c990e13f
Johnny Bhojwani <johnnyvb@gmail.com>
2024-02-18 23:18:18 -0600

    changes for Spring 2024

--- processStudents.py
+++ processStudents.py
@@ -3,14 +3,22 @@
     To accomplish this, read the file - students.csv into the program. The program
     should evaluate the GPA to see if it is higher or lower than 3.0. If it is,
     then it should be written out to the file - processedStudents.csv. (This file
-    already contains headers and the headers should NOT be overwritten.) 
+    already contains headers and the headers should NOT be overwritten.)
 
-*   Create a dictionary of each student where the student ID is the key
-    and the GPA is the value.
+*   Create a VE and name it quiz_ve. Install the 3rd party libraries- wordcloud and imageio.
+    Make sure your VE is NOT part of your repository. Make sure your .gitignore
+    IS part of your repo. The code will produce a heart-shaped wordcloud of the most
+    frequently appearing words in the text of Romeo and Juliet.
 
-*  print out the dictionary
+*   Create a dictionary of each student where the student FULL NAME (proper casing) is the key
+    and the GPA is the value. 
 
-*  print out the corresponding GPA for student - 567890123
+*   print out the dictionary
+
+*   print out the corresponding GPA for student - Luke Brazzi
+
+*   push your repo to GitHub. Only your VE should not be in your repo. Everything else should be
+    pushed. Submit your Github repo URL in the response field of the quiz.
 
 I have outlined comments for each step of the program. You are
 not required to use them but it is provided to help you work
@@ -43,14 +51,15 @@ import csv
 
 #use a loop to iterate through each row of the file
 
+
     #check if the GPA is below 3.0. If so, write the record to the outfile
     
         
 
 
 
-    # append the record to the dictionary with the student id as the Key
-    # and the value as the GPA
+    # append the record to the dictionary with the student Full name in proper case 
+    # as the Key and the value as the GPA
     
 
 
@@ -60,15 +69,30 @@ import csv
 #print the entire dictionary
 
 
-#Print the student id 
-
-
-#print out the corresponding GPA from the dictionary
+#Print the corresponding GPA for student 'Luke Brazzi'
 
 
 
 #close the outfile
 
+
+
+
+
+#display the wordcloud
+from pathlib import Path
+from wordcloud import WordCloud
+import imageio.v2 as imageio
+import matplotlib.pyplot as plt
+
+
+text = Path("RomeoAndJuliet.txt").read_text()
+mask_image = imageio.imread("mask_heart.png")
+wordcloud = WordCloud(colormap="prism", mask=mask_image, background_color="white")
+wordcloud = wordcloud.generate(text)
+wordcloud = wordcloud.to_file("RomeoAndJulietHeart.png")
+plt.imshow(wordcloud)
+plt.show()
 
 
 
